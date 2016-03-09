702226dbbbd9918840f66f6564594af1c20753ba
fuzongjian <2917066078@qq.com>
2016-03-09 15:47:24 +0800

    初始化项目

---
+++ fuzongjian/fuzongjian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fuzongjian
+//
+//  Created by fdkj0002 on 16/3/9.
+//  Copyright © 2016年 FZJ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fuzongjian/fuzongjian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fuzongjian
+//
+//  Created by fdkj0002 on 16/3/9.
+//  Copyright © 2016年 FZJ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
